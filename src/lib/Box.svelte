56a9eb8c70b2d6959651c8dd57dfd83c5b130cac
willuhmjs <willuhmjs@gmail.com>
2023-04-08 13:13:40 -0400

    feat: basic print info box

---
+++ src/lib/Box.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let title: String;
+</script>
+<section>
+    <h2>{title}</h2>
+    <div class="box">
+        <slot />
+    </div>
+</section>
+<style>
+    .box {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 1em;
+        margin: 1em 0;
+        background-color: #f5f5f5;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,17 @@
 	import { estimateCost, formatCost } from '../lib/calculator';
 	import type { PrintConditions, Printer } from '../lib/printers';
 	import { getPrinters } from '../lib/printers';
+	
+	import Box from '$lib/Box.svelte';
 
 	let fixedTo = 2;
 
-	let conditions: PrintConditions = {
+	let printTimeMinutes: number = 4;
+	let printTimeHours: number = 10;
+
+	$: conditions = {
 		amountInGrams: 4,
-		printTime: 0.36,
+		printTime: printTimeHours + (printTimeMinutes/60),
 		warmupTime: 5,
 		electricityCost: 0.11,
 		costOfFilament: 25,
@@ -22,11 +27,32 @@
 </script>
 
 <main>
-	...
+	<Box title="Print Info">
+		<div>
+			<label>Print Weight</label>
+			<input type="number" bind:value={conditions.amountInGrams} />
+		</div>
+		<div>
+			<label>Print Time</label>
+			<input type="number" bind:value={printTimeHours} />
+			<input type="number" bind:value={printTimeMinutes} />
+		</div>
+		<div>
+			<label>Warm Up Time</label>
+			<input type="number" bind:value={conditions.warmupTime} />
+		</div>
+		<div>
+			<label>Electricity Cost</label>
+			<input type="number" bind:value={conditions.electricityCost} />
+		</div>
+		<div>
+			<label>Cost of Filament</label>
+			<input type="number" bind:value={conditions.costOfFilament} />
+		</div>
+		<div>
+			<label>Other Costs</label>
+			<input type="number" bind:value={conditions.otherCosts} />
+		</div>
+	</Box>
+	{estimateCost(conditions, printer)}
 </main>
-<style>
-	main {
-		max-width: 600px;
-		margin: 0 auto;
-	}
-</style>
