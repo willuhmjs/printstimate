27f103d5113a39d108285e96f6247df0811c96c3
willuhmjs <willuhmjs@gmail.com>
2023-04-08 13:33:23 -0400

    feat: style the box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,31 +28,72 @@
 
 <main>
 	<Box title="Print Info">
-		<div>
+		<div class="inputItem">
 			<label for="amountInGrams">Print Weight</label>
-			<input id="amountInGrams" type="number" bind:value={conditions.amountInGrams} />
+			<br>
+			<div class="inputContent">
+				<input id="amountInGrams" type="number" bind:value={conditions.amountInGrams} />
+			</div>
 		</div>
-		<div>
+		<div class="inputItem">
 			<label for="printTime">Print Time</label>
-			<input class="printTime" type="number" bind:value={printTimeHours} />
-			<input class="printTime" type="number" bind:value={printTimeMinutes} />
+			<br>
+			<div class="inputContent">
+				<input class="printTime" type="number" bind:value={printTimeHours} />
+				<input class="printTime" type="number" bind:value={printTimeMinutes} />
+			</div>
 		</div>
-		<div>
+		<div class="inputItem">
 			<label for="warmupTime">Warm Up Time</label>
-			<input id="warmupTime" type="number" bind:value={conditions.warmupTime} />
+			<br>
+			<div class="inputContent">
+				<input id="warmupTime" type="number" bind:value={conditions.warmupTime} />
+			</div>
 		</div>
-		<div>
+		<div class="inputItem">
 			<label for="electricityCost">Electricity Cost</label>
-			<input id="electricityCost" type="number" bind:value={conditions.electricityCost} />
+			<br>
+			<div class="inputContent">
+				<input id="electricityCost" type="number" bind:value={conditions.electricityCost} />
+			</div>
 		</div>
-		<div>
+		<div class="inputItem">
 			<label for="costOfFilament">Cost of Filament</label>
-			<input id="costOfFilament" type="number" bind:value={conditions.costOfFilament} />
+			<br>
+			<div class="inputContent">
+				<input id="costOfFilament" type="number" bind:value={conditions.costOfFilament} />
+			</div>
 		</div>
-		<div>
+		<div class="inputItem">
 			<label for="otherCosts">Other Costs</label>
-			<input id="otherCosts" type="number" bind:value={conditions.otherCosts} />
+			<br>
+			<div class="inputContent">
+				<input id="otherCosts" type="number" bind:value={conditions.otherCosts} />
+			</div>
 		</div>
 	</Box>
 	{estimateCost(conditions, printer)}
 </main>
+<style>
+	.inputContent {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.inputContent input {
+		flex: 1;
+		padding: 1ch;
+		border: 1px solid #ccc;
+		border-radius: 4px;
+		margin-top: 0.6em;
+	}
+
+	.inputContent input:nth-child(2) {
+		margin-left: 1ch;
+	}
+
+	.inputItem {
+		margin-bottom: 1em;
+	}
+
+</style>
