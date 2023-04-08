17e1e4b3972878a06d4f1d79ef9d202de620bf3f
willuhmjs <willuhmjs@gmail.com>
2023-04-08 13:15:30 -0400

    feat: accessibility a11y

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,29 +29,29 @@
 <main>
 	<Box title="Print Info">
 		<div>
-			<label>Print Weight</label>
-			<input type="number" bind:value={conditions.amountInGrams} />
+			<label for="amountInGrams">Print Weight</label>
+			<input id="amountInGrams" type="number" bind:value={conditions.amountInGrams} />
 		</div>
 		<div>
-			<label>Print Time</label>
-			<input type="number" bind:value={printTimeHours} />
-			<input type="number" bind:value={printTimeMinutes} />
+			<label for="printTime">Print Time</label>
+			<input class="printTime" type="number" bind:value={printTimeHours} />
+			<input class="printTime" type="number" bind:value={printTimeMinutes} />
 		</div>
 		<div>
-			<label>Warm Up Time</label>
-			<input type="number" bind:value={conditions.warmupTime} />
+			<label for="warmupTime">Warm Up Time</label>
+			<input id="warmupTime" type="number" bind:value={conditions.warmupTime} />
 		</div>
 		<div>
-			<label>Electricity Cost</label>
-			<input type="number" bind:value={conditions.electricityCost} />
+			<label for="electricityCost">Electricity Cost</label>
+			<input id="electricityCost" type="number" bind:value={conditions.electricityCost} />
 		</div>
 		<div>
-			<label>Cost of Filament</label>
-			<input type="number" bind:value={conditions.costOfFilament} />
+			<label for="costOfFilament">Cost of Filament</label>
+			<input id="costOfFilament" type="number" bind:value={conditions.costOfFilament} />
 		</div>
 		<div>
-			<label>Other Costs</label>
-			<input type="number" bind:value={conditions.otherCosts} />
+			<label for="otherCosts">Other Costs</label>
+			<input id="otherCosts" type="number" bind:value={conditions.otherCosts} />
 		</div>
 	</Box>
 	{estimateCost(conditions, printer)}
