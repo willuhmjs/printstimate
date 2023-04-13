3929284e5c730dc1d91536fd976ca1b2cd5b455b
willuhmjs <willuhmjs@gmail.com>
2023-04-12 22:57:30 -0400

    feat: meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
 	import './style.css';
 </script>
+<svelte:head>
+	<title>printstimate - 3D printing cost calculator</title>
+	<meta name="title" content="printstimate - 3D printing cost calculator">
+	<meta name="description" content="A tool for calculating the cost of 3D printing / additive manufacturing jobs.">
+	<meta name="theme-color" content="#fcfcfc">
+
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://willuhmjs.github.io/printstimate/">
+	<meta property="og:title" content="printstimate - 3D printing cost calculator">
+	<meta property="og:description" content="A tool for calculating the cost of 3D printing / additive manufacturing jobs.">
+
+	<meta property="twitter:card" content="summary">
+	<meta property="twitter:url" content="https://willuhmjs.github.io/printstimate/">
+	<meta property="twitter:title" content="printstimate - 3D printing cost calculator">
+	<meta property="twitter:description" content="A tool for calculating the cost of 3D printing / additive manufacturing jobs.">
+</svelte:head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <slot />
