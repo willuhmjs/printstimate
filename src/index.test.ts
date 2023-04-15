309bbf864b96fb21c2c9479f967e30124045fa1c
willuhmjs <willuhmjs@gmail.com>
2023-04-14 21:01:01 -0400

    fix(tests): use default data

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,22 +8,22 @@ describe('basic calculator test', () => {
 	const cost = estimateCost(printConditions, KP3S);
 
 	it('estimates the cost of a Kingroon KP3S print', () => {
-		expect(cost).toBe(0.10275000000000001);
+		expect(cost).toBe(0.37775);
 	})
 
 	it('rounds the cost to 2 decimal places', () => {
-		expect(formatCost(cost, 2)).toBe('$0.10');
+		expect(formatCost(cost, 2)).toBe('$0.38');
 	});
 
 	it('rounds the cost to 3 decimal places', () => {
-		expect(formatCost(cost, 3)).toBe('$0.103');
+		expect(formatCost(cost, 3)).toBe('$0.378');
 	});
 
 	it('rounds the cost to 4 decimal places', () => {
-		expect(formatCost(cost, 4)).toBe('$0.1028');
+		expect(formatCost(cost, 4)).toBe('$0.3777');
 	});
 
 	it('rounds the cost to 5 decimal places', () => {
-		expect(formatCost(cost, 5)).toBe('$0.10275');
+		expect(formatCost(cost, 5)).toBe('$0.37775');
 	});
 });
